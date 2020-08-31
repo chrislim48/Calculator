dab9e0d397d55e7816de20331c93ca1bbaceb18e
chrislim48 <christopherlim4800@gmail.com>
2020-08-31 02:50:54 -0700

    Update calculator_main.cpp

--- calculator_main.cpp
+++ calculator_main.cpp
@@ -6,11 +6,11 @@ int main() {
 	double num1, num2, total;
 	string symbol;
   
-  	Calculator C;
+	Calculator C;
   
  	 // Display all the Calculator Functions 
 	C.displayFunctions();
-  
+
 	while(isProgramRunning == true) {
 
 		cout << "Enter the symbol in the parenthesis of the function you would like to use: ";
@@ -22,27 +22,27 @@ int main() {
 			C.add(num1, num2, total);
 		}
 		else if(symbol == "-") {
-	    	C.askForTwoNum(num1, num2);
-	    	C.subtract(num1, num2, total);
-	    }
+			C.askForTwoNum(num1, num2);
+			C.subtract(num1, num2, total);
+		}
 		else if(symbol == "*") {
-		  	C.askForTwoNum(num1, num2);
-	    	C.multiply(num1, num2, total);
+			C.askForTwoNum(num1, num2);
+	   		C.multiply(num1, num2, total);
+		}
+		else if(symbol == "/") {
+	    	C.askForTwoNum(num1, num2);
+	    	C.divide(num1, num2, total);
+		}
+		else if(symbol == "?") {
+	    	C.askForTwoNum(num1, num2);
+	    	C.find_remainder(num1, num2, total);
+		}
+		else if(symbol == "exit") { // if the user writes 'exit' as their symbol, it will make the while loop end and stop the calculator program
+	    	isProgramRunning = false;
 	    }
-	    else if(symbol == "/") {
-	      	C.askForTwoNum(num1, num2);
-	      	C.divide(num1, num2, total);
-	    }
-	    else if(symbol == "?") {
-	      	C.askForTwoNum(num1, num2);
-	     	C.find_remainder(num1, num2, total);
-	    }
-	    else if(symbol == "exit") { // if the user writes 'exit' as their symbol, it will make the while loop end and stop the calculator program
-	      	isProgramRunning = false;
-	    }
-	    else {
-	      	cout << "ERROR: Symbol not found.\n"; // If it's not on the list of function symbols, then it will say it's not a valid symbol and ask for another one
-	    }
+		else {
+			cout << "ERROR: Symbol not found.\n"; // If it's not on the list of function symbols, then it will say it's not a valid symbol and ask for another one
+		}
     
 	}
 	
